648e89a9c05ebae703cadc90db962c4e79730174
Giovanni Arcangeli <gio22arcangeli@gmail.com>
2023-01-11 10:59:45 +0100

    Modifica metodo autenticazione in AutenticazioneDAO e aggiunta di getAlbergatorebyCF(anche utente)

--- src/main/java/BookNow/Storage/AutenticazioneDAO.java
+++ src/main/java/BookNow/Storage/AutenticazioneDAO.java
@@ -15,16 +15,18 @@ public class AutenticazioneDAO {
             if(ut.getPassword().equals(password) && ut.getUsername().equals(username))
                 if(ut.isAlbergatore()==1){
                     AlbergatoreDAO a=new AlbergatoreDAO();
-                    ArrayList<Albergatore> albergatori=a.getAllAlbergatori();
+                   /* ArrayList<Albergatore> albergatori=a.getAllAlbergatori();
                     for(Albergatore al: albergatori){
                         if(al.getCf().equals(ut.getCf())) return al;
-                    }
+                    }*/
+                    return a.getAlbergatoreByCF(ut.getCf());
                 }else{
                     ClienteDAO c=new ClienteDAO();
-                    ArrayList<Cliente> clienti=c.getAllClienti();
+                   /* ArrayList<Cliente> clienti=c.getAllClienti();
                     for(Cliente cl: clienti){
                         if(cl.getCf().equals(ut.getCf())) return cl;
-                    }
+                    }*/
+                    return c.getClienteByCf(ut.getCf());
                 }
         }
         return null;
