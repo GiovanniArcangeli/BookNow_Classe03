91b7bd8d8f4338affe3931ee938c0bed118d4549
Giovanni Arcangeli <gio22arcangeli@gmail.com>
2023-01-18 12:30:19 +0100

    Aggiunta test ControlloAccessoTest

--- src/test/java/ControlloAccessoTest.java
+++ src/test/java/ControlloAccessoTest.java
@@ -7,28 +7,28 @@ import static org.junit.Assert.assertTrue;
 public class ControlloAccessoTest {
     @Test
     public void soloUsernameErrato(){
-        String username=null; String password=("12345678");
+        String username="abc"; String password="12345678";
         boolean check=StorageFacade.getInstance().controlloAccesso(username, password) instanceof Utente;
         assertFalse(check);
     }
 
     @Test
     public void soloPasswordErrata(){
-        String username="CarmineRen"; String password=null;
+        String username="CarmineRen"; String password="abc";
         boolean check=StorageFacade.getInstance().controlloAccesso(username, password) instanceof Utente;
         assertFalse(check);
     }
 
     @Test
     public void passEUsErrate(){
-        String username=null; String password=null;
+        String username="abc"; String password="abc";
         boolean check=StorageFacade.getInstance().controlloAccesso(username, password) instanceof Utente;
         assertFalse(check);
     }
 
     @Test
     public void autenticato(){
-        String username="CarmineRen"; String password=("Carmine4");
+        String username="CarmineRen"; String password="Carmine4";
         boolean check=StorageFacade.getInstance().controlloAccesso(username, password) instanceof Utente;
         assertTrue(check);
     }
