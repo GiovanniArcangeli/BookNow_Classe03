2bbf32688920ec0a6dd1dfe62e3f34f0e548b8be
LukaszGajewski8 <l.gajewski@studenit.unisa.it>
2023-01-18 12:46:20 +0100

    Javascript bug fixes

--- src/main/webapp/PrenotaStanzaGUI/FormCercaStruttura.js
+++ src/main/webapp/PrenotaStanzaGUI/FormCercaStruttura.js
@@ -14,21 +14,25 @@ function FormCercaStruttura() {
         $("#numOspiti").focus();
         return false;
     } else if (!dataIn.match(rxDatePattern)) {
-        if(((meseIn == 1 || meseIn == 3 || meseIn == 5 || meseIn == 7 || meseIn == 8 || meseIn == 10 || meseIn == 12) && giornoIn > 31) ||
+        alert("La data di check-in deve essere nel formato aaaa/mm/dd.");
+        $("#dataIn").focus();
+        return false;
+    } else if(((meseIn == 1 || meseIn == 3 || meseIn == 5 || meseIn == 7 || meseIn == 8 || meseIn == 10 || meseIn == 12) && giornoIn > 31) ||
             ((meseIn == 4 || meseIn == 6 || meseIn == 9 || meseIn== 11) & giornoIn > 30) ||
             (annoIn%4 != 0 && meseIn == 2 && giornoIn > 28) || (annoIn%4 == 0 && meseIn == 2 && giornoIn > 29)) {
-            alert("La data di check-in deve essere nel formato aaaa/mm/dd.");
+            alert("La data di check-in deve essere valida.");
             $("#dataIn").focus();
             return false;
-        }
     } else if (!dataOut.match(rxDatePattern)) {
-        if(((meseOut == 1 || meseOut == 3 || meseOut == 5 || meseOut == 7 || meseOut == 8 || meseOut == 10 || meseOut == 12) && giornoOut > 31) ||
+        alert("La data di check-out deve essere nel formato aaaa/mm/dd.");
+        $("#dataOut").focus();
+        return false;
+    } else if(((meseOut == 1 || meseOut == 3 || meseOut == 5 || meseOut == 7 || meseOut == 8 || meseOut == 10 || meseOut == 12) && giornoOut > 31) ||
             ((meseOut == 4 || meseOut == 6 || meseOut == 9 || meseOut== 11) & giornoOut > 30) ||
             (annoOut%4 != 0 && meseOut == 2 && giornoOut > 28) || (annoOut%4 == 0 && meseOut == 2 && giornoOut > 29)) {
             alert("La data di check-out deve essere nel formato aaaa/mm/dd.");
             $("#dataOut").focus();
             return false;
-        }
     } else if (Date.parse(dataIn) - Date.parse(dataOut) > 0) {
         alert("La data di check-out deve essere successiva alla data di check-in.");
         $("#dataOut").focus();
